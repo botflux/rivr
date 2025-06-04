06895be0643bc897dd79d5f4ab7047e152d31b7b
Victor <victor.mendele68@gmail.com>
2025-06-04 21:24:54 +0200

    test competing consumers

--- packages/engine-redis/src/sandbox.ts
+++ packages/engine-redis/src/sandbox.ts
@@ -5,52 +5,53 @@ import {rivr} from "rivr";
 
 
 async function start () {
-  const engine = createEngine({
-    redis: { url: "redis://localhost:6379" },
-  })
-
-  const worker = engine.createWorker()
-  const trigger = engine.createTrigger()
-
-  const w = rivr.workflow<number>("calc")
-    .step({
-      name: "add-1",
-      handler: ({ state }) => state + 1
-    })
-    .addHook("onWorkflowCompleted", (w, s) => {
-      console.log("state", s)
-    })
-
-  console.log("starting")
-  await worker.start([ w ])
-
-  console.log("triggering")
-  await trigger.trigger(w, 9)
-  await trigger.trigger(w, 10)
-  await trigger.trigger(w, 11)
-  await trigger.trigger(w, 12)
-  await trigger.trigger(w, 13)
-  await trigger.trigger(w, 14)
-
-  // const client = createClient({
-  //   url: "redis://localhost:6379",
+  // const engine = createEngine({
+  //   redis: { url: "redis://localhost:6379" },
   // })
   //
-  // await client.connect()
+  // const worker = engine.createWorker()
+  // const trigger = engine.createTrigger()
   //
-  // console.log("waiting")
-  // consumer(client)
-  // await producer(client, "hello 1")
-  // await producer(client, "hello 2")
-  // await producer(client, "hello 3")
-  // await producer(client, "hello 4")
-  // // console.log("waiting 5s...")
-  // // await setTimeout(5_000)
-  // // console.log("finish waiting")
+  // const w = rivr.workflow<number>("calc")
+  //   .step({
+  //     name: "add-1",
+  //     handler: ({ state }) => state + 1
+  //   })
+  //   .addHook("onWorkflowCompleted", (w, s) => {
+  //     console.log("state", s)
+  //   })
   //
+  // console.log("starting")
+  // await worker.start([ w ])
   //
-  // await producer(client, "hello 5")
-  // await producer(client, "hello 6")
+  // console.log("triggering")
+  // await trigger.trigger(w, 9)
+  // await trigger.trigger(w, 10)
+  // await trigger.trigger(w, 11)
+  // await trigger.trigger(w, 12)
+  // await trigger.trigger(w, 13)
+  // await trigger.trigger(w, 14)
+
+  const client = createClient({
+    url: "redis://localhost:6379",
+  })
+
+  await client.connect()
+
+  console.log("waiting")
+  consumer(client)
+  consumer(client)
+  await producer(client, "hello 1")
+  await producer(client, "hello 2")
+  await producer(client, "hello 3")
+  await producer(client, "hello 4")
+  // console.log("waiting 5s...")
+  // await setTimeout(5_000)
+  // console.log("finish waiting")
+
+
+  await producer(client, "hello 5")
+  await producer(client, "hello 6")
 
 }
 
@@ -58,15 +59,20 @@ async function producer (client: ReturnType<typeof createClient>, message: strin
   await client.lPush("foo", message)
 }
 
+let consumerId = 1
+
 async function consumer (client: ReturnType<typeof createClient>) {
+
+  const localId = consumerId ++
+
   try {
     while (true) {
       const value = await client.brPop("foo", 1)
 
       if (value) {
-        console.log("new message", value)
+        console.log(localId, "new message", value)
       } else {
-        console.log("no new message")
+        console.log(localId, "no new message")
       }
     }
   } catch (error) {
