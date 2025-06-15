501a89e651dff56d55e698ef87f7f95e85c4e470
Victor <victor.mendele68@gmail.com>
2025-06-15 18:26:54 +0200

    remove tasks
    I went to the conclusion that it added bloat to
    add a task flow when a workflow with one step is enough.

--- packages/engine-redis/src/sandbox.ts
+++ packages/engine-redis/src/sandbox.ts
@@ -1,44 +1,6 @@
-import {createQueue} from "./redis";
-import {trigger, tasks, createWorker} from "rivr";
-import {createTask} from "rivr/dist/task/types";
 
 async function sandbox () {
-  const queue = createQueue({
-    redis: {
-      url: "redis://localhost:6379"
-    }
-  })
 
-  type UserCreated = {
-    type: "user_created"
-    email: string
-  }
-
-  const task = createTask<UserCreated>("notify_user")
-    .decorate("sendMail", async (email: string, content: string) => console.log(
-      `Sending ${content} to ${email}`
-    ))
-    .handler({
-      handler: async ({ state, task }) => {
-        await task.sendMail(state.email, "User created successfully")
-      }
-    })
-
-  const worker = createWorker({
-    primary: queue,
-    tasks: [ task ]
-  })
-
-  await worker.start()
-
-  await trigger(
-    queue,
-    task,
-    {
-      type: "user_created",
-      email: "foo@bar.com",
-    }
-  )
 }
 
 sandbox().catch(console.error)
