e642e2e1fd3063614e244d63f4032df39fb1ac2f
Victor <victor.mendele68@gmail.com>
2025-06-23 12:43:30 +0200

    create a connection wrapper

--- packages/engine-rabbitmq/src/rabbitmq.resilience.ts
+++ packages/engine-rabbitmq/src/rabbitmq.resilience.ts
@@ -5,6 +5,8 @@ import {Network, StartedNetwork} from "testcontainers";
 import {createQueue} from "./rabbitmq";
 import {randomUUID} from "node:crypto";
 import {installUnhandledRejectionHook, Message} from "rivr";
+import {ChannelModel, connect} from "amqplib";
+import {createConnection} from "node:net";
 
 installUnhandledRejectionHook()
 
@@ -92,6 +94,106 @@ describe('rabbitmq resilience test suite', function () {
       // Then
       const mError = await queue.produce([ randomMessage() ]).catch(e => e)
       t.assert.match((mError as Error).message, /closed/, `${(mError as Error)?.message} does not match '/closed/'`)
+    })
+  })
+
+  describe('connection wrapper', function () {
+    class ConnectionWrapper {
+      #createConnection: () => Promise<ChannelModel>
+      #connection: ChannelModel | undefined
+      #closed = true
+
+      constructor(createConnection: () => Promise<ChannelModel>) {
+        this.#createConnection = createConnection;
+      }
+
+      async getConnection(): Promise<ChannelModel> {
+        if (this.#connection === undefined) {
+          this.#connection = await this.#createConnection()
+          this.#connection.on("error", () => {})
+          this.#connection.on("close", () => {
+            this.#closed = true
+          })
+          this.#closed = false
+        }
+
+        if (this.#closed) {
+          this.#connection = undefined
+          return await this.getConnection()
+        }
+
+        return this.#connection
+      }
+
+      async disconnect(): Promise<void> {
+        try {
+          await this.#connection?.close()
+        } catch (e: unknown) {
+          if (!this.#isUnexpectedCloseError(e)) {
+            throw e
+          }
+        }
+        this.#connection = undefined
+      }
+
+      #isUnexpectedCloseError(error: unknown): boolean {
+        if (!this.#isError(error)) {
+          return false
+        }
+
+        const possibilities = [
+          "Unexpected close",
+          "Channel closed",
+          /Connection closed/
+        ]
+
+        return possibilities.some(element => (error.message as string).match(element))
+      }
+
+      #isError(error: unknown): error is Error {
+        return typeof error === "object" && error !== null
+          && "message" in error && typeof error.message === "string"
+      }
+    }
+
+    test("should be able to reconnect", async (t: TestContext) => {
+      // Given
+      const proxy = await toxiproxy.createProxy({
+        name: `rabbitmq-${randomUUID()}`,
+        enabled: true,
+        upstream: "rabbitmq:5672"
+      })
+
+      t.after(() => proxy.instance.remove())
+
+      const wrapper = new ConnectionWrapper(() => connect(`amqp://${proxy.host}:${proxy.port}`))
+
+      t.after(async () => {
+        await wrapper.disconnect()
+      })
+
+      const conn1 = await wrapper.getConnection()
+      const ch1 = await conn1.createConfirmChannel()
+
+      await ch1.assertQueue("foo")
+
+      // When
+      await proxy.setEnabled(false)
+      const mError1 = await ch1.assertQueue("foo")
+        .then(() => {})
+        .catch(e => e)
+
+      await proxy.setEnabled(true)
+
+      const conn2 = await wrapper.getConnection()
+      const ch2 = await conn2.createConfirmChannel()
+
+      // Then
+      const mError2 = await ch2.assertQueue("foo")
+        .then(() => {})
+        .catch(e => e)
+      t.assert.strictEqual(mError2, undefined)
+      t.assert.match((mError1 as Error)?.message, /Channel closed/)
     })
   })
 })
